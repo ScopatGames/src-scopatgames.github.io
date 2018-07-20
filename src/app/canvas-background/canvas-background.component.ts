d82b1501770a2a9d26e4200775d2cedcc9f393cc
Scott Patten <scopatgames@gmail.com>
2018-07-20 09:19:27 -0700

    templated out layout; background canvas resizing

--- src/app/canvas-background/canvas-background.component.ts
+++ src/app/canvas-background/canvas-background.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvasBackgroundComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(){
+    this.draw();
+  }
+
+  /**
+   * @desc draw step for canvas
+   */
+  draw = () => {
+    let canvas = <HTMLCanvasElement> document.getElementById('canvas-background');
+    let ctx = canvas.getContext('2d');
+
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    ctx.clearRect(0, 0, screen.width, screen.height);
+
+    ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
+    ctx.strokeRect(10.5, 10, 10, 10);
+    // ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    // ctx.fillRect(10.5, 10, 10, 10);
+
+    window.requestAnimationFrame(this.draw);
+    
   }
 
 }
