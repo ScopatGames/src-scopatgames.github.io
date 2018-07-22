a30437033d4192f9fa804150da147d83d2869357
Scott Patten <scopatgames@gmail.com>
2018-07-22 10:09:49 -0700

    animated canvases

--- src/app/canvas-background/canvas-background.component.ts
+++ src/app/canvas-background/canvas-background.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./canvas-background.component.css']
 })
 export class CanvasBackgroundComponent implements OnInit {
-
-  constructor() {}
+  timeZero : Date;
+  numberOfBackgroundParticles : number = 100;
+  speedOfBackgroundParticles : number = 0.03; 
+  seedArray : number[] = [];
 
   ngOnInit(){
+    this.timeZero = new Date();
+    for(let i = 0; i < this.numberOfBackgroundParticles; i++){
+      this.seedArray.push(Math.random());
+    }
     this.draw();
   }
 
@@ -17,18 +23,35 @@ export class CanvasBackgroundComponent implements OnInit {
    * @desc draw step for canvas
    */
   draw = () => {
-    let canvas = <HTMLCanvasElement> document.getElementById('canvas-background');
-    let ctx = canvas.getContext('2d');
+    const canvas = <HTMLCanvasElement> document.getElementById('canvas-background');
+    const ctx = canvas.getContext('2d');
 
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 
-    ctx.clearRect(0, 0, screen.width, screen.height);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
-    ctx.strokeRect(10.5, 10, 10, 10);
-    // ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-    // ctx.fillRect(10.5, 10, 10, 10);
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+
+    const time = new Date();
+    const xRange = ctx.canvas.width;
+    const yRange = ctx.canvas.height;
+
+    this.seedArray.forEach( (seed, index) => {
+      ctx.save();
+      const xLocation = this.seedArray[this.numberOfBackgroundParticles - 1 - index] * xRange;
+      const timeDelta = time.getTime() - this.timeZero.getTime();
+      const yLocation = (timeDelta * this.speedOfBackgroundParticles + seed * yRange ) % yRange;
+      ctx.translate(xLocation, yLocation);
+      // const rotationDirection = seed < 0.5 ? 1 : -1;
+      // ctx.rotate((2 * Math.PI / 2 * time.getSeconds() + 2 * Math.PI / 2000 * time.getMilliseconds() + seed)*rotationDirection);
+      // ctx.drawImage(this.smoke, -16, -16);
+      const randomAlpha = Math.random()*1;
+      ctx.fillStyle = 'rgba(255, 96, 176, ' + randomAlpha + ')';
+
+      ctx.fillRect(-1.5, -1.5, 3, 3);
+      ctx.restore();
+    });      
 
     window.requestAnimationFrame(this.draw);
     
--- src/app/graphic/graphic.component.ts
+++ src/app/graphic/graphic.component.ts
@@ -6,10 +6,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphic.component.css']
 })
 export class GraphicComponent implements OnInit {
-
-  constructor() { }
+  graphic : HTMLImageElement;
+  smoke : HTMLImageElement;
+  timeZero : Date;
+  numberOfTrailParticles : number = 100;
+  speedOfTrailParticles : number = 0.1; 
+  seedArray : number[] = [];
 
   ngOnInit() {
+    this.graphic = new Image();
+    this.graphic.src = '../../assets/img/triangle.png';
+
+    this.smoke = new Image();
+    this.smoke.src = '../../assets/img/smoke.png';
+    
+    this.timeZero = new Date();
+    for(let i = 0; i < this.numberOfTrailParticles; i++){
+      this.seedArray.push(Math.random());
+    }
+
+    // Initiate drawing on canvas
+    this.draw();
+  }
+
+  draw = () => {
+    const canvas = <HTMLCanvasElement> document.getElementById('canvas-graphic');
+    const ctx = canvas.getContext('2d');
+
+    ctx.canvas.width = 200;
+    ctx.canvas.height = window.innerHeight/2 + 48;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    const time = new Date();
+
+    const medianXLocation = ctx.canvas.width/2;
+    const xRange = this.graphic.width/6;
+    const maxYLocation = ctx.canvas.height +10 - this.graphic.height;
+
+    this.seedArray.forEach( (seed, index) => {
+      ctx.save();
+      const xOffset = this.seedArray[this.numberOfTrailParticles - 1 - index] * xRange - xRange/2;
+      const xLocation = medianXLocation + xOffset;
+      const timeDelta = time.getTime() - this.timeZero.getTime();
+      const yOffset = (timeDelta * this.speedOfTrailParticles + seed * maxYLocation ) % maxYLocation;
+      const yLocation = this.graphic.height - 10 + yOffset;
+      ctx.translate(xLocation, yLocation);
+      const rotationDirection = seed < 0.5 ? 1 : -1;
+      ctx.rotate((2 * Math.PI / 2 * time.getSeconds() + 2 * Math.PI / 2000 * time.getMilliseconds() + seed)*rotationDirection);
+      ctx.drawImage(this.smoke, -16, -16);
+      const randomAlpha = Math.random()*1;
+      ctx.strokeStyle = 'rgba(255, 223, 176, ' + randomAlpha +')';
+
+      // ctx.strokeRect(-3, -3, 6, 6);
+      ctx.beginPath();
+      ctx.moveTo(0,-5);
+      ctx.lineTo(4.33, 2.5);
+      ctx.lineTo(-4.33, 2.5);
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+    });      
+
+    ctx.drawImage(this.graphic, medianXLocation - this.graphic.width/2, 0);
+
+    // Request the next animation frame
+    window.requestAnimationFrame(this.draw);
+
   }
 
 }
